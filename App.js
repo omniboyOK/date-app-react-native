ecb9f45965a490a2484768b2d6ed63483dfd7e2e
omniboyok <barrea.pablo@gmail.com>
2021-05-15 18:12:44 -0300

    Adding branding theme

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import * as eva from "@eva-design/eva";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import MainView from "./src/Main";
+import { default as theme } from './custom-theme.json';
 
 export default () => {
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
+      <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
         <MainView />
       </ApplicationProvider>
     </>
