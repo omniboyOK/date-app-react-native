ee959547a32b601d33bf7deeb6ea297845bc72f1
omniboyok <barrea.pablo@gmail.com>
2021-07-09 21:50:58 -0300

    feat: constant import of profile pics

---
+++ src/constants/pictures.js
@@ -0,0 +1,10 @@
+export const PICTURES = {
+  ben: require("../../assets/faces/ben.png"),
+  betty: require("../../assets/faces/betty.png"),
+  bobby: require("../../assets/faces/bobby.png"),
+  pili: require("../../assets/faces/pili.png"),
+  robert: require("../../assets/faces/robert.png"),
+  susan: require("../../assets/faces/susan.png"),
+  will: require("../../assets/faces/will.png"),
+  default: require("../../assets/faces/placeholder.jpg"),
+};
