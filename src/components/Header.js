4f467e1af78d75e946a88fb7cfc1fe34bd08e499
omniboyok <barrea.pablo@gmail.com>
2021-07-09 21:50:01 -0300

    fix: visual changes on header

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Image
+        source={require("../../assets/Isologo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#ffffff",
+    width: "100%",
+    height: 80,
+    paddingTop: 26,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 10,
+    borderBottomWidth: 0,
+
+    // shadow
+    shadowColor: "#8b53ff",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
+
+export default Header;
