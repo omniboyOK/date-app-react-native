9c51e231e8a288cc9d44cd4f2bbec501f2036242
omniboyok <barrea.pablo@gmail.com>
2021-07-17 17:44:06 -0300

    custom fonst and loading

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Dimensions, Platform, SafeAreaView, StyleSheet } from "react-native";
 import NavigationView from "./src/navigation/NavigationView";
+import fonts from "./src/constants/fonts";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default () => {
+  const [fontsLoaded] = useFonts(fonts);
+  
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <>
       <SafeAreaView style={styles.container}>
---
+++ src/constants/fonts.js
@@ -0,0 +1,11 @@
+const fonts = {
+  "Pangram-Black": require("../../assets/fonts/Pangram-Black.otf"),
+  "Pangram-Bold": require("../../assets/fonts/Pangram-Bold.otf"),
+  "Pangram-ExtraBold": require("../../assets/fonts/Pangram-ExtraBold.otf"),
+  "Pangram-ExtraLight": require("../../assets/fonts/Pangram-ExtraLight.otf"),
+  "Pangram-Medium": require("../../assets/fonts/Pangram-Medium.otf"),
+  "Pangram-Regular": require("../../assets/fonts/Pangram-Regular.otf"),
+  "Pangram-Light": require("../../assets/fonts/Pangram-Light.otf"),
+};
+
+export default fonts;
