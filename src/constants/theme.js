0a41a873691159be8f6fd76b10d7fe668f83cfcf
omniboyok <barrea.pablo@gmail.com>
2021-07-17 15:55:32 -0300

    theme variables

---
+++ src/constants/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    tundrLight: "#B68CFF",
+    tundrMain: "#754ADB",
+    tundrStrong: "#5633B7",
+    snowWhite: "#FFFFFF",
+    blackHearth: "#000000"
+  },
+  radius: {
+    light: 7,
+    medium: 17,
+    strong: 30,
+  },
+};
+
+export default theme;
